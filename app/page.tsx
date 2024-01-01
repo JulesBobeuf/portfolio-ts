fbfbafafe14b55a8ab5d40be185851c48de3fd6a
Bobeuf Jules <jules_bobeuf@ens.univ-artois.fr>
2024-01-01 20:37:16 +0100

    [A] added env variables to make the site easier to maintain

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <Navbar/>
       <main className="flex min-h-screen flex-col items-center justify-between p-8">
         <div className="z-10 max-w-5xl w-full items-center justify-between font-mono lg:flex">
-          <Avatar size={256} src={"/avatar/choix.jpg"}></Avatar>
+          <Avatar size={process.env.AVATAR_SIZE ? +process.env.AVATAR_SIZE : 256} src={`${process.env.AVATAR_URL}`}></Avatar>
         </div>
         <div className="">
           <p>cc</p>
