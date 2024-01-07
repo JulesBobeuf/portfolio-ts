91d0ca1d00b9cee93c03ad552e72e016063a94db
Bobeuf Jules <jules_bobeuf@ens.univ-artois.fr>
2024-01-07 21:34:34 +0100

    [A] Started implementing the article page for projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import Image from "next/image";
 import { Avatar } from "./Avatar";
-import Navbar from "./Navbar/navbar";
 
 export default function Home() {
   return (
     <>
-      {" "}
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono lg:flex">
         <Avatar
           size={process.env.AVATAR_SIZE ? +process.env.AVATAR_SIZE : 256}
