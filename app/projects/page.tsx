66217f42ec20ec08ca01f226574b188425d088f8
Bobeuf Jules <jules_bobeuf@ens.univ-artois.fr>
2024-01-12 18:35:54 +0100

    [A] List of Projects page is now responsive

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,12 @@
 import listPropProjectCard, { PropProjectCard } from "./dataProjects";
 import { ProjectCard } from "./projectCard";
+import styles from "./projects.module.css";
 
 export default function Home() {
   return (
     <>
       <h1 className="m-8 text-4xl">Projects</h1>
-      <div className="grid grid-cols-4 gap-6 my-8">
+      <div className={styles.projects}>
         {listPropProjectCard.map((prop: PropProjectCard) => (
           <ProjectCard
             key={prop.title}
