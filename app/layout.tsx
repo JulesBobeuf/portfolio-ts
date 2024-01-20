bc09dfba72ccbad902fe4f5b9c31c10c368f859d
Bobeuf Jules <jules_bobeuf@ens.univ-artois.fr>
2024-01-20 11:58:58 +0100

    [A] Added print stylesheet

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { Navbar } from './Navbar'
 import { Footer } from './Footer'
+import print from "./print.module.css";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,11 +21,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>
+        <div className={print.noprint}>
+          <Navbar/>
+        </div>
         <main className="flex min-h-screen flex-col items-center justify-between p-2 md:p-4 lg:p-8 mx-2 md:mx-20 lg:mx-40 bg-gray-900">
         {children}
         </main>
-        <Footer/>
+        <div className={print.noprint}>
+          <Footer/>
+        </div>
       </body>
     </html>
   )
