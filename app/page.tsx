76a30a581022ee538c739b4a1331a373019f39b3
Bobeuf Jules <jules_bobeuf@ens.univ-artois.fr>
2024-09-03 13:52:33 +0200

    [U] Updated homepage: Certifications, Added Online Courses, Info about me

--- app/page.tsx
+++ app/page.tsx
@@ -16,20 +16,11 @@ export default function Home() {
         <h2 className={styles.subtitle}>About me</h2>
           <p className={styles.text}> Hello! My name is Jules, I&apos;m a 21 year old French man that currently studies computer science!</p>
           <p className={styles.text}> On this website, you can find all the relevant information you may need about what I do in IT. However, here is a little resume of mine: </p>
+          <p className={styles.text}> I am currently persuing a Master's degree in Software Engineering at the University of Calais, France.</p>
+          <p className={styles.text}> I graduated from my Bachelor's degree at the University Institute of Technology of Lens, France in 2024. </p>
           <p className={styles.text}> I graduated from high school with Mathematics and English Specialties with distinctions in 2021. </p>
           <p className={styles.text}>I also obtained my end of study certificate in music theory with honors in 2019. </p>
           <p className={styles.text}>During my studies, I have explored all fields in Computer Science, but programming applications has been my favorite. </p>
-
-          <hr className="my-8"></hr>
-
-          <h2 className={styles.subtitle}>Certifications</h2>
-
-          <a className={styles.link} href="https://www.udemy.com/certificate/UC-174b96a6-078f-46c1-a580-dd6db84f6d4a/">C# Masterclass</a>
-          <p className={styles.text}>Udemy course about C#, WPF, Unity, Linq, and .NET as a whole.</p>
-          <a className={styles.link} href="https://www.udemy.com/certificate/UC-07ef669a-f88b-4d13-8450-79f3513e8e51/">Ruby For Beginners</a>
-          <p className={styles.text}>Udemy course about the basics of Ruby.</p>
-          <a className={styles.link} href="https://pix.fr/">Pix : P-CT2W4QJF</a>
-          <p className={styles.text}>Pix is a public French service that certifies French Students&apos; numerical skills.</p>
 
           <hr className="my-8"></hr>
 
@@ -40,6 +31,34 @@ export default function Home() {
           <hr className="my-8"></hr>
           
           <Work></Work>
+
+          <hr className="my-8"></hr>
+
+          <h2 className={styles.subtitle}>Certifications/Test Scores</h2>
+
+          <a className={styles.link} href="https://www.ets.org/toefl.html">TOEFL iBT 107/120 (C1)</a>
+          <p className={styles.text}>TOEFL is a full english test made by ETS.</p>
+          <a className={styles.link} href="https://www.ets.org/toeic.html">TOEIC 970/990 (C1)</a>
+          <p className={styles.text}>TOEIC is an english test made by ETS.</p>
+          <a className={styles.link} href="https://pix.fr/">Pix : P-CT2W4QJF</a>
+          <p className={styles.text}>Pix is a public French service that certifies French Students&apos; numerical skills.</p>
+          
+          <hr className="my-8"></hr>
+
+          <h2 className={styles.subtitle}>Online Courses</h2>
+
+          <a className={styles.link} href="https://www.udemy.com/certificate/UC-174b96a6-078f-46c1-a580-dd6db84f6d4a/">C# Masterclass</a>
+          <p className={styles.text}>Udemy course about C#, WPF, Unity, Linq, and .NET as a whole.</p>
+          <a className={styles.link} href="https://www.udemy.com/certificate/UC-8c44e76c-05ff-478c-ae68-cb037c69458b/">Spring Framework</a>
+          <p className={styles.text}>Udemy course about the core of Spring.</p>
+          <a className={styles.link} href="https://www.udemy.com/certificate/UC-2e5c5972-3072-493c-8e3f-ea009ab62c20/">SpringBoot Fundamentals</a>
+          <p className={styles.text}>Udemy course about SpringBoot: How it works, it's core principles and good practice.</p>
+          <a className={styles.link} href="https://www.udemy.com/certificate/UC-ca877fa5-8928-48f2-9009-adee38a2ec4e/">SpringBoot RestAPI</a>
+          <p className={styles.text}>Udemy course about making proper RestAPIs using SpringBoot.</p>
+          <a className={styles.link} href="https://www.udemy.com/certificate/UC-a5a4d28c-6844-4859-9526-ffb735925b8b/">Maven Crash Course</a>
+          <p className={styles.text}>Udemy course about how Maven works, it's core principles and good practice.</p>
+          <a className={styles.link} href="https://www.udemy.com/certificate/UC-969fc0e2-cf49-4b7b-b461-4821dc9ca37e/">The Complete Haskell Course</a>
+          <p className={styles.text}>Udemy course about Haskell, functionnal programming, and a little bit of mathematics and data structures!</p>
       </div>
     </div>
   );
